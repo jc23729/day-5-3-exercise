401bf7692fc25e4738cef27b8900e5ee1dcf9b98
Juan Cruz <jc23729@gmail.com>
2020-12-05 22:06:50 +0000

    added notes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #Write your code below this row 👇
 
 total = 0
+#starting at 2, first even number, up to 1oo have to put 101, increment by 2
 for number in range(2, 101 ,2):
   total += number
 print(total)
